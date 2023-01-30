046350f4ba7ba0fb605659e4af86406cceb66af2
56778gdagk <91785115+56778gdagk@users.noreply.github.com>
2023-01-30 19:17:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 music_1 = "";
 music_2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload()
 {
     music_1 = loadSound("Music_1");
@@ -14,9 +20,31 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw()
 {
     image(video,0,0,600,500);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
 }
